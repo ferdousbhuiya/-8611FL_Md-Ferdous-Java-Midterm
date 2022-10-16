855317edd1b5fbe3afb32db25862da5ec11e1ecd
ferdousbhuiya <ferdousbhuiya@gmail.com>
2022-10-15 21:49:55 -0400

    Bubble sort

--- src/sorting/SortEfficiency.java
+++ src/sorting/SortEfficiency.java
@@ -26,25 +26,27 @@ public class SortEfficiency {
 
     public static void main(String[] args) throws Exception {
         // Declare and initialize an array of a desired length with random numbers (Try this with 100, 1000, 10000, 100000)
-        int[] numberArray = new int[100];
+        int[] numberArray = new int[1000];
         insertRandomNumbersIntoArray(numberArray);
 
         SortingAlgorithms sort = new SortingAlgorithms();
-        SharedStepsDatabase ssdb = new SharedStepsDatabase();
+        SharedStepsDatabase sharedStepsDatabase = new SharedStepsDatabase();
 
         // region Selection Sort
         numberArray = sort.selectionSort(numberArray);
         long selectionSortExecutionTime = sort.executionTime;
 
-        System.out.println("***SELECTION SORT***\nArray Length: " + numberArray.length + "\nExecution Time: "
+        System.out.println("\n\n============================-- SELECTION SORT --============================\nArray Length: " + numberArray.length + "\nExecution Time: "
                 + selectionSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Selection Sort took: "
+                + selectionSortExecutionTime + " milliseconds\n");
 
         // Insert sorted array into a database table, with the desired table name and column name
-        ssdb.insertIntegerArray("selection_sort", "sorted_numbers", numberArray);
+        sharedStepsDatabase.insertIntegerArray("selection_sort", "sorted_numbers", numberArray);
 
         // Retrieve all elements from the newly created table
         String query = "SELECT * FROM SELECTION_SORT";
-        List<String> sorted_numbers = ssdb.executeQueryReadAllSingleColumn(query, "sorted_numbers");
+        List<String> sorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query, "sorted_numbers");
         printValue(sorted_numbers);
 
         // endregion
@@ -55,47 +57,121 @@ public class SortEfficiency {
         // region Insertion Sort
         numberArray = sort.insertionSort(numberArray);
         long insertionSortExecutionTime = sort.executionTime;
-
-        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Insertion Sort took: "
+        System.out.println("\n\n=============================- INSERTION SORT -=============================\nArray Length: " + numberArray.length + "\nExecution Time: "
                 + insertionSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Insertion Sort took: "
+                + insertionSortExecutionTime + " milliseconds\n");
+        sharedStepsDatabase.insertIntegerArray("insertion_sort", "insertionSorted_numbers", numberArray);
+        String query1 = "SELECT * FROM insertion_sort";
+        List<String> insertionSorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query1, "insertionSorted_numbers");
+        printValue(insertionSorted_numbers);
 
         // endregion
 
         randomize(numberArray);
 
         // region Bubble Sort
-
+        numberArray = sort.bubbleSort(numberArray);
+        long bubbleSortExecutionTime = sort.executionTime;
+        System.out.println("\n\n=============================- BUBBLE SORT -=============================\nArray Length: " + numberArray.length + "\nExecution Time: "
+                + bubbleSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Bubble Sort took: "
+                + bubbleSortExecutionTime + " milliseconds\n");
+        sharedStepsDatabase.insertIntegerArray("bubble_sort", "bubbleSorted_numbers", numberArray);
+        String query2 = "SELECT * FROM bubble_sort";
+        List<String> bubbleSorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query2, "bubbleSorted_numbers");
+        printValue(bubbleSorted_numbers);
         // endregion
 
         randomize(numberArray);
 
         // region Merge Sort
+        numberArray = sort.mergeSort(numberArray, 0, numberArray.length - 1);
+        long mergeSortExecutionTime = sort.executionTime;
 
+        System.out.println("\n\n=============================- MERGE SORT -=============================\nArray Length: " + numberArray.length + "\nExecution Time: "
+                + mergeSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Merge Sort took: "
+                + mergeSortExecutionTime + " milliseconds\n");
+        sharedStepsDatabase.insertIntegerArray("merge_sort", "mergeSorted_numbers", numberArray);
+        String query3 = "SELECT * FROM merge_sort";
+        List<String> mergeSorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query3, "mergeSorted_numbers");
+        printValue(mergeSorted_numbers);
         // endregion
 
         randomize(numberArray);
 
         // region Quick Sort
+        numberArray = sort.quickSort(numberArray, 0, numberArray.length - 1);
+        long quickSortExecutionTime = sort.executionTime;
 
+        System.out.println("\n\n=============================- QUICK SORT -=============================\nArray Length: " + numberArray.length + "\nExecution Time: "
+                + quickSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Quick Sort took: "
+                + quickSortExecutionTime + " milliseconds\n");
+        sharedStepsDatabase.insertIntegerArray("quick_sort", "quickSorted_numbers", numberArray);
+        String query4 = "SELECT * FROM quick_sort";
+        List<String> quickSorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query4, "quickSorted_numbers");
+        printValue(quickSorted_numbers);
         // endregion
 
         randomize(numberArray);
 
         // region Heap Sort
-
+        numberArray = sort.heapSort(numberArray);
+        long heapSortExecutionTime = sort.executionTime;
+        System.out.println("\n\n=============================- HEAP SORT -=============================\nArray Length: " + numberArray.length + "\nExecution Time: "
+                + heapSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Heap Sort took: "
+                + heapSortExecutionTime + " milliseconds\n");
+        sharedStepsDatabase.insertIntegerArray("heap_sort", "heapSorted_numbers", numberArray);
+        String query5 = "SELECT * FROM heap_sort";
+        List<String> heapSorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query5, "heapSorted_numbers");
+        printValue(heapSorted_numbers);
         // endregion
 
         randomize(numberArray);
 
         // region Bucket Sort
-
+        numberArray = sort.bucketSort(numberArray, SortingAlgorithms.maximumValue(numberArray));
+        long bucketSortExecutionTime = sort.executionTime;
+        System.out.println("\n\n=============================- BUCKET SORT -=============================\nArray Length: " + numberArray.length + "\nExecution Time: "
+                + bucketSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Bucket Sort took: "
+                + bucketSortExecutionTime + " milliseconds\n");
+        sharedStepsDatabase.insertIntegerArray("bucket_sort", "bucketSorted_numbers", numberArray);
+        String query6 = "SELECT * FROM bucket_sort";
+        List<String> bucketSorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query6, "bucketSorted_numbers");
+        printValue(bucketSorted_numbers);
         // endregion
 
         randomize(numberArray);
 
         // region Shell Sort
-
+        numberArray = sort.shellSort(numberArray, numberArray.length);
+        long shellSortExecutionTime = sort.executionTime;
+        System.out.println("\n\n=============================- SHELL SORT -=============================\nArray Length: " + numberArray.length + "\nExecution Time: "
+                + shellSortExecutionTime + " milliseconds");
+        System.out.println("Total Execution Time of " + numberArray.length + " numbers in Shell Sort took: "
+                + shellSortExecutionTime + " milliseconds\n");
+        sharedStepsDatabase.insertIntegerArray("shell_sort", "shellSorted_numbers", numberArray);
+        String query7 = "SELECT * FROM shell_sort";
+        List<String> shellSorted_numbers = sharedStepsDatabase.executeQueryReadAllSingleColumn(query7, "shellSorted_numbers");
+        printValue(shellSorted_numbers);
         // endregion
+
+
+        System.out.println("======----- Comparison of the sorting times for the same numbers of element -----==========");
+        System.out.println("Total number of Element : "+numberArray.length);
+        System.out.println("Execution time for Selection Sort: "+selectionSortExecutionTime+" milliseconds.");
+        System.out.println("Execution time for Insertion Sort: "+insertionSortExecutionTime+" milliseconds.");
+        System.out.println("Execution time for Bubble Sort: "+bubbleSortExecutionTime+" milliseconds.");
+        System.out.println("Execution time for Merge Sort: "+mergeSortExecutionTime+" milliseconds.");
+        System.out.println("Execution time for Quick Sort: "+quickSortExecutionTime+" milliseconds.");
+        System.out.println("Execution time for Heap Sort: "+heapSortExecutionTime+" milliseconds.");
+        System.out.println("Execution time for Bucket Sort: "+bucketSortExecutionTime+" milliseconds.");
+        System.out.println("Execution time for Shell Sort: "+shellSortExecutionTime+" milliseconds.");
+
     }
 
     // region Helper Methods
--- src/sorting/SortingAlgorithms.java
+++ src/sorting/SortingAlgorithms.java
@@ -3,86 +3,216 @@ package sorting;
 public class SortingAlgorithms {
 
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * You must implement all of the sorting algorithms below. Feel free to add any helper methods that you may need,
      * but make sure they are private, as to not be accessed outside of this class.
-     *
+     * <p>
      * You must also store the sorted arrays into their own individual database tables (Selection Sort should be stored
-     *  in table `selection_sort`, Insertion Sort should be stored in table `insertion_sort`)
+     * in table `selection_sort`, Insertion Sort should be stored in table `insertion_sort`)
      */
 
     long executionTime = 0;
 
     public static void printSortedArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        for (int k = 0; k < array.length; k++) {
+            System.out.println(array[k]);
         }
     }
-
     public int[] selectionSort(int[] array) {
         final long startTime = System.currentTimeMillis();
 
-        for (int i = 0; i < array.length - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < array.length; j++) {
+        for (int m = 0; m < array.length - 1; m++) {
+            int min = m;
+            for (int j = m + 1; j < array.length; j++) {
                 if (array[j] < array[min])
                     min = j;
             }
-
             int temp = array[min];
-            array[min] = array[i];
-            array[i] = temp;
+            array[min] = array[m];
+            array[m] = temp;
         }
-
         final long endTime = System.currentTimeMillis();
         this.executionTime = endTime - startTime;
-
         return array;
     }
 
     public int[] insertionSort(int[] array) {
         final long startTime = System.currentTimeMillis();
         // IMPLEMENT HERE
+        int n = array.length;
+        for (int ii = 1; ii < n; ++ii) {
+            int key = array[ii];
+            int ji = ii - 1;
 
+            while (ji >= 0 && array[ji] > key) {
+                array[ji + 1] = array[ji];
+                ji = ji - 1;
+            }
+            array[ji + 1] = key;
+        }
         final long endTime = System.currentTimeMillis();
         this.executionTime = endTime - startTime;
-
         return array;
     }
 
     public int[] bubbleSort(int[] array) {
+        final long startTime = System.currentTimeMillis();
         // IMPLEMENT HERE
-
+        int n = array.length;
+        for (int ib = 0; ib < n - 1; ib++)
+            for (int jb = 0; jb < n - ib - 1; jb++)
+                if (array[jb] > array[jb + 1]) {
+                    int temp = array[jb];
+                    array[jb] = array[jb + 1];
+                    array[jb + 1] = temp;
+                }
+        final long endTime = System.currentTimeMillis();
+        this.executionTime = endTime - startTime;
         return array;
     }
 
-    public int[] mergeSort(int[] array) {
+    public int[] mergeSort(int[] array, int start, int arrayLength) {
+        final long startTime = System.currentTimeMillis();
         // IMPLEMENT HERE
-
+        if (start < arrayLength) {
+            int ms = start + (arrayLength - start) / 2;
+            mergeSort(array, start, ms);
+            mergeSort(array, ms + 1, arrayLength);
+            merge(array, start, ms, arrayLength);
+        }
+        final long endTime = System.currentTimeMillis();
+        this.executionTime = endTime - startTime;
         return array;
     }
-
-    public int[] quickSort(int[] array) {
+    private void merge(int arr[], int lm, int mm, int rm) {
+        int nm1 = mm - lm + 1;
+        int nm2 = rm - mm;
+        int Lm[] = new int[nm1];
+        int Rm[] = new int[nm2];
+        for (int i = 0; i < nm1; ++i)
+            Lm[i] = arr[lm + i];
+        for (int jm = 0; jm < nm2; ++jm)
+            Rm[jm] = arr[mm + 1 + jm];
+        int im = 0, jm = 0;
+        int k = lm;
+        while (im < nm1 && jm < nm2) {
+            if (Lm[im] <= Rm[jm]) {
+                arr[k] = Lm[im];
+                im++;
+            } else {
+                arr[k] = Rm[jm];
+                jm++;
+            }
+            k++;
+        }
+        while (im < nm1) {
+            arr[k] = Lm[im];
+            im++;
+            k++;
+        }
+        while (jm < nm2) {
+            arr[k] = Rm[jm];
+            jm++;
+            k++;
+        }
+    }
+    public int[] quickSort(int[] array, int low, int high) {
+        final long startTime = System.currentTimeMillis();
         // IMPLEMENT HERE
 
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+        final long endTime = System.currentTimeMillis();
+        this.executionTime = endTime - startTime;
         return array;
     }
-
+   private int partition(int intArray[], int low, int high) {
+        int pi = intArray[high];
+        int iq = (low - 1);
+        for (int jq = low; jq < high; jq++) {
+            if (intArray[jq] <= pi) {
+                iq++;
+                int temp = intArray[iq];
+                intArray[iq] = intArray[jq];
+                intArray[jq] = temp;
+            }
+        }
+        int temp = intArray[iq + 1];
+        intArray[iq + 1] = intArray[high];
+        intArray[high] = temp;
+        return iq + 1;
+    }
     public int[] heapSort(int[] array) {
+        final long startTime = System.currentTimeMillis();
         // IMPLEMENT HERE
-
+        int N = array.length;
+        for (int ih = N / 2 - 1; ih >= 0; ih--)
+            heapify(array, N, ih);
+        for (int ih = N - 1; ih > 0; ih--) {
+            int temp = array[0];
+            array[0] = array[ih];
+            array[ih] = temp;
+            heapify(array, ih, 0);
+        }
+        final long endTime = System.currentTimeMillis();
+        this.executionTime = endTime - startTime;
         return array;
     }
-
-    public int[] bucketSort(int[] array) {
-        //implement here
-
-        return array;
+    private void heapify(int arr[], int N, int i) {
+        int largest = i;
+        int lh = 2 * i + 1;
+        int rh = 2 * i + 2;
+        if (lh < N && arr[lh] > arr[largest])
+            largest = lh;
+        if (rh < N && arr[rh] > arr[largest])
+            largest = rh;
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, N, largest);
+        }
     }
-
-    public int[] shellSort(int[] array) {
+    public int[] bucketSort(int[] array, int maximum_value) {
+        final long startTime = System.currentTimeMillis();
+        int[] newbucket = new int[maximum_value + 1];
+        int[] sorted_array = new int[array.length];
+        for (int b = 0; b < array.length; b++)
+            newbucket[array[b]]++;
+        int position = 0;
+        for (int bb = 0; bb < newbucket.length; bb++)
+            for (int cb = 0; cb < newbucket[bb]; cb++)
+                sorted_array[position++] = bb;
+        final long endTime = System.currentTimeMillis();
+        this.executionTime = endTime - startTime;
+        return sorted_array;
+    }
+    public static int maximumValue(int[] array) {
+        int maximum_value = 0;
+        for (int db = 0; db < array.length; db++)
+            if (array[db] > maximum_value)
+                maximum_value = array[db];
+        return maximum_value;
+    }
+    public int[] shellSort(int[] array, int n) {
+        final long startTime = System.currentTimeMillis();
         //implement here
-
+        for (int interval = n / 2; interval > 0; interval /= 2) {
+            for (int is = interval; is < n; is += 1) {
+                int temp = array[is];
+                int js;
+                for (js = is; js >= interval && array[js - interval] > temp; js -= interval) {
+                    array[js] = array[js - interval];
+                }
+                array[js] = temp;
+            }
+        }
+        final long endTime = System.currentTimeMillis();
+        this.executionTime = endTime - startTime;
         return array;
     }
 }
